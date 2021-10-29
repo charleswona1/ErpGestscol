e5a58984b7372e594ab48c21506d9318b5f77a1b
charleswona1 <muriel.wona@abyster.com>
2021-10-29 17:33:25 +0100

    flashback

--- resources/views/components/flash-back.blade.php
+++ resources/views/components/flash-back.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
-</div>
+@if (Session::has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {!! Session::get('success') !!}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+@if (Session::has('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {!! Session::get('error') !!}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+@if (Session::has('warning'))
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        {!! Session::get('warning') !!}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+@if (Session::has('info'))
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        {!! Session::get('info') !!}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
